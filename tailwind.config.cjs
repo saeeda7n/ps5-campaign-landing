74041f8fb2ffa52f100c110ee927a7f59f3eb379
Saeed1112 <nullee.sa@gmail.com>
2023-08-01 11:41:35 +0330

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			screens:{
+				xs:'348px'
+			},
 			fontFamily:{
 				'kalameh': ['KalamehWebFaNum', 'sans-serif']
 			},
